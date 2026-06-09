69a496deb19ac684fa807f653349e22642fcee92
Xtupp <alexaballayg@gmail.com>
2026-06-09 16:05:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ if uploaded_file is not None:
             st.error(f"Faltan columnas en el CSV: {missing}")
             st.stop()
 
-        predictions   = model.predict(df[FEATURES])
-        probabilities = model.predict_proba(df[FEATURES])[:, 1]  # prob. de abandono
+        predictions   = model.predict(data)
+        probabilities = model.predict_proba(data)[:, 1]  # prob. de abandono
 
         df["Predicción"]         = predictions
         df["Prob. Abandono (%)"] = (probabilities * 100).round(1)
